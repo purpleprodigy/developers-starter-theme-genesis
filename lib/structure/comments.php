37b2e1d095e8d65ccc5cdc99134319678f2859a3
Rose Cox <rose@purpleprodigy.com>
2016-07-26 14:49:07 +1000

    Initial commit

---
+++ lib/structure/comments.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Comments structure handling.
+ *
+ * @package     PurpleProdigy\Developers
+ * @since       1.0.0
+ * @author      Purple Prodigy
+ * @link        http://www.purpleprodigy.com
+ * @licence     GNU General Public License 2.0+
+ */
+namespace PurpleProdigy\Developers;
+
+add_filter( 'genesis_comment_list_args', __NAMESPACE__ . '\setup_comments_gravatar' );
+/**
+ * Modify size of the Gravatar in the entry comments
+ *
+ * @since 1.0.0
+ *
+ * @param array $args
+ *
+ * @return mixed
+ */
+function setup_comments_gravatar( array $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
