fec3d3f7a016f8fce18e8c0ae4d2cec354c99e07
Rose Cox <rose@purpleprodigy.com>
2016-10-20 12:58:20 +1000

    Fix responsive menu not loading

--- lib/functions/enqueue-assets.php
+++ lib/functions/enqueue-assets.php
@@ -23,7 +23,7 @@ function enqueue_assets() {
 	wp_enqueue_style( CHILD_TEXT_DOMAIN . '-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
-	wp_enqueue_script( CHILD_TEXT_DOMAIN . '-responsive-menu', CHILD_URL . '/js/responsive-menu.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_enqueue_script( CHILD_TEXT_DOMAIN . '-responsive-menu', CHILD_URL . '/assets/js/responsive-menu.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 	$localized_script_args = array(
 		'mainMenu' => __( 'Menu', CHILD_TEXT_DOMAIN ),
 		'subMenu'  => __( 'Menu', CHILD_TEXT_DOMAIN ),
